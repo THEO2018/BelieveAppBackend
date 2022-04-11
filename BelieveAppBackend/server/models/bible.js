1a5a620c4a3877b066d5c9d85865816a05f64954
Pavneet Singh <pavneet.singh@netsetsoftware.com>
2022-04-11 15:20:50 +0530

    change in add betrothed

--- BelieveAppBackend/server/models/bible.js
+++ BelieveAppBackend/server/models/bible.js
@@ -27,9 +27,19 @@ module.exports.getBibleLink = function (params, callback) {
 }
 
 module.exports.editBibleLink = function (params, callback) {
-    Bible.findByIdAndUpdate(params.bible_id, { bible_url: params.bible_url }, { new: true }, callback);
+    var update = clean(params)
+    Bible.findByIdAndUpdate(params.bible_id, update, { new: true }, callback);
 }
 
 module.exports.deleteBibleUrl = function (id, callback) {
     Bible.findByIdAndUpdate(id, { status: 'D' }, { new: true }, callback);
 }
+
+function clean(obj) {
+    for (var propName in obj) {
+      if (obj[propName] === null || obj[propName] === '' || obj[propName] === undefined) {
+        delete obj[propName];
+      }
+    }
+    return obj;
+  }
